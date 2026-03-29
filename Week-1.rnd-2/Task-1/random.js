a6c09ed347c23de3747e1962a6cf58a014400167
Vengadesan <vengadesan8061@gmail.com>
2026-03-29 20:21:46 +0530

    Round-2-week-1

--- Week-1.rnd-2/Task-1/random.js
+++ Week-1.rnd-2/Task-1/random.js
@@ -2,5 +2,4 @@ function getRandomQuote(quotes) {
     const index = Math.floor(Math.random() * quotes.length);
     console.log(` Quote of the Day: ${quotes[index]}`);
 }
-
 module.exports = getRandomQuote;
